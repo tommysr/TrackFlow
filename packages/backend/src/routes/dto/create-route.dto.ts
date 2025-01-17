91ce05c4eff22f47b683d2033051673d297cd45f
tommysr <47206288+tommysr@users.noreply.github.com>
2025-01-17 18:51:20 +0100

    dto for route creation

---
+++ packages/backend/src/routes/dto/create-route.dto.ts
@@ -0,0 +1,34 @@
+import { IsArray, ArrayNotEmpty, ValidateNested, IsEnum, IsNumber, IsDate, Matches } from 'class-validator';
+import { Type } from 'class-transformer';
+import { LocationDto } from 'src/shipments/dto/create-shipment.dto';
+
+export enum RouteOperationType {
+  PICKUP = 'pickup', // not used
+  DELIVERY = 'delivery', // not used
+  BOTH = 'both'
+}
+
+export class ShipmentRouteOperation {
+  @IsNumber()
+  id: number;
+
+  @IsEnum(RouteOperationType)
+  @Matches(/^(both)$/) // TODO: add pickup and delivery
+  type: RouteOperationType;
+}
+
+export class CreateRouteDto {
+  @IsArray()
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => ShipmentRouteOperation)
+  shipments: ShipmentRouteOperation[];
+
+  @ValidateNested()
+  @Type(() => LocationDto)
+  carrierLocation: LocationDto;
+
+  @IsDate()
+  @Type(() => Date)
+  date: Date;
+} 
