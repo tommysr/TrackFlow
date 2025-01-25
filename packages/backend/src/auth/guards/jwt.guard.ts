4c4f1189027f62d6c32214742c68f8d9ddd55791
tommysr <47206288+tommysr@users.noreply.github.com>
2025-01-25 10:28:32 +0100

    silence logging

--- packages/backend/src/auth/guards/jwt.guard.ts
+++ packages/backend/src/auth/guards/jwt.guard.ts
@@ -7,17 +7,17 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   private readonly logger = new Logger(JwtAuthGuard.name);
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    this.logger.log('JWT Guard - canActivate called');
+    // this.logger.log('JWT Guard - canActivate called');
     const request = context.switchToHttp().getRequest();
-    this.logger.log('Headers:', request.headers);
+    // this.logger.log('Headers:', request.headers);
     return super.canActivate(context);
   }
 
   handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
-    this.logger.log('JWT Guard - handleRequest called');
-    this.logger.log('Error:', JSON.stringify(err));
-    this.logger.log('User:', JSON.stringify(user));
-    this.logger.log('Info:', JSON.stringify(info));
+    // this.logger.log('JWT Guard - handleRequest called');
+    // this.logger.log('Error:', JSON.stringify(err));
+    // this.logger.log('User:', JSON.stringify(user));
+    // this.logger.log('Info:', JSON.stringify(info));
 
     if (err || !user) {
       this.logger.error('JWT Guard - Authentication failed', { err, info });
